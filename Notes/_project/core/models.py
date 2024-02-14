0693b67b03f4da4553555b292cf07db6a81c7daa
Joseph Ditton <jditton.atomic@gmail.com>
2024-02-14 10:59:42 -0700

    class

--- Notes/_project/core/models.py
+++ Notes/_project/core/models.py
@@ -5,6 +5,10 @@ class Note(models.Model):
     title = models.TextField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['title'])
+        ]
 
 
 class Task(models.Model):
